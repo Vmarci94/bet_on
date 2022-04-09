4bc41a4fd55b3896ffbc038877246bb511643449
Vmarci94 <vamrci94@gmail.com>
2022-04-09 20:34:56 +0200

    init android project

---
+++ app/src/main/java/hu/bme/beton/MainActivity.kt
@@ -0,0 +1,18 @@
+package hu.bme.beton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.beton.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
